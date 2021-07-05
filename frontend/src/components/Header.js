8ffe451b3fb0696f3eab2f8f8661c9c771244e5f
Yamendra Pun <punyamendra@gmail.com>
2021-07-05 23:03:23 +0900

    Fix some bugs and remove unwanted files

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,12 +1,12 @@
 import React from 'react'
 
 const Header = () => {
-    return (
-        <>
-         <h1>Hangman</h1>   
-         <p>Find the hidden word - Enter a letter</p>   
-        </>
-    )
+  return (
+    <>
+      <h1>Hangman Game</h1>
+      <p>Find the hidden word - Enter a letter</p>
+    </>
+  )
 }
 
 export default Header
